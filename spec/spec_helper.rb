6edb72376b4a93d589089e5a4a1177d725cca223
Nagra, Harmanjit <harmanjit.nagra@accenture.com>
2017-03-24 13:52:05 -0700

    test commit 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# Test Comments
+# Test Comments 1
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
